3852dcd2e3897a3799a4e1c4139dda9b7b074aba
Dogiino <sudo.dogitrp@gmail.com>
2026-04-25 13:31:19 -0600

    Update NelvkarAnime.py

--- NelvkarAnime.py
+++ NelvkarAnime.py
@@ -1,1 +1,10 @@
-print("XD")
+import tkinter
+
+Ventana = tkinter.Tk()
+
+Ventana.title("NelvkarAnime")
+Ventana.geometry("650x500+100+100")
+Ventana.resizable(False,False)
+Ventana.configure(bg="#202020")
+
+Ventana.mainloop()
